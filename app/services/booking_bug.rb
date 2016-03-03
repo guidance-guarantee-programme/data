f073c80b1a9fee59da11eb1c7f55ff3e746df4e6
David Henry <dw_henry@yahoo.com.au>
2016-03-03 15:51:24 +0000

    Remove abstract Config implementation and replace with Concrete implementation within BookingBug namespace.

--- app/services/booking_bug.rb
+++ app/services/booking_bug.rb
@@ -1,2 +1,20 @@
 class BookingBug
+  class Config
+    ATTRIBUTES = %w(environment company_id api_key app_id email password).freeze
+    attr_accessor(*ATTRIBUTES)
+
+    def initialize
+      yield(self) if block_given?
+    end
+
+    def attributes
+      ATTRIBUTES
+    end
+  end
+
+  def self.config
+    @config ||= BookingBug::Config.new
+    yield(@config) if block_given?
+    @config
+  end
 end
