5e2d6b090acc0dd56b79e16bf6b8b1755eaf8b38
Andrew Garner <andrew@andrewgarner.com>
2016-03-07 09:19:41 +0000

    Seed database with date dimensions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'date'
+
+PopulateDateDimension.new(begin_date: Date.new(2015, 1, 1), end_date: Date.today).call
