021a94b0842b91da92749502680134fc629d6574
David Henry <dw_henry@yahoo.com.au>
2016-03-10 15:51:42 +0000

    Change BOOKINGBUG_ENVIRONMENT to BOOKING_BUG_DOMAIN env variable to support bookings.pensionwise.gov.uk

--- app/services/booking_bug.rb
+++ app/services/booking_bug.rb
@@ -1,6 +1,6 @@
 class BookingBug
   class Config
-    ATTRIBUTES = %w(environment company_id api_key app_id email password).freeze
+    ATTRIBUTES = %w(domain company_id api_key app_id email password).freeze
     attr_accessor(*ATTRIBUTES)
 
     def initialize
