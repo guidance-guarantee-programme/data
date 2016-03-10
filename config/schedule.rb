f5581cb9ff9ae45e9a4413b1c2c275c6c28765cf
Andrew Garner <andrew@andrewgarner.com>
2016-03-10 11:59:38 +0000

    Populate date dimension daily at midnight

--- config/schedule.rb
+++ config/schedule.rb
@@ -14,6 +14,11 @@ module PensionWise
       error_handler do |error|
         Bugsnag.notify_or_ignore(error)
       end
+
+      every(1.day, 'dimensions.date', at: '00:05') do
+        PopulateDateDimensionJob.perform_later(begin_date: Time.zone.yesterday.to_s,
+                                               end_date: Time.zone.today.to_s)
+      end
     end
   end
 end
