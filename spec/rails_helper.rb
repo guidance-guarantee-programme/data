e21ce6c50b3dd3c361b92ac362f8a8dad0f26e54
Andrew Garner <andrew@andrewgarner.com>
2016-03-10 12:14:57 +0000

    Add staging environment to application

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require_relative '../config/environment'
 
+abort('The Rails environment is running in staging mode!') if Rails.env.staging?
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'spec_helper'
